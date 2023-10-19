a01971d958e59e0250d3e2177e2c974d312a70d7
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-20 04:40:25 +0530

    add authcontroller class to api gateway

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/api/AuthController.java
@@ -0,0 +1,15 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/20/2023
+  @ Time         : 4:39 AM
+*/
+package lk.nexttravel.api_gateway.api;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/20/2023
+ * Time    : 4:39 AM
+ */
+public class AuthController {
+}
