032764ec14aacc31cd5ccfcde6beced448090a64
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-20 04:34:17 +0530

    implimenting security config class to api gateway

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/entity/SecurityConfig.java
@@ -0,0 +1,16 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/20/2023
+  @ Time         : 4:32 AM
+*/
+package lk.nexttravel.api_gateway.entity;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/20/2023
+ * Time    : 4:32 AM
+ */
+
+public class SecurityConfig {
+}
