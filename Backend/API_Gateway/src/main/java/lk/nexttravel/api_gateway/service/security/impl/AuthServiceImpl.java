062dfa20708c62afea31f03eae170726ec4eee8c
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-20 04:53:50 +0530

    add AuthServiceImpl class in the api gateway

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/service/security/impl/AuthServiceImpl.java
@@ -0,0 +1,20 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/20/2023
+  @ Time         : 4:52 AM
+*/
+package lk.nexttravel.api_gateway.service.security.impl;
+
+import lk.nexttravel.api_gateway.service.security.AuthService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/20/2023
+ * Time    : 4:52 AM
+ */
+
+@Service
+public class AuthServiceImpl implements AuthService {
+}
