181163bab2b3ca9c6823317a337b51410702da62
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-20 06:25:09 +0530

    fix errors frontend before signup_page to check username api gateway

--- Frontend/pages/assets/js/signup_page_js.js
+++ Frontend/pages/assets/js/signup_page_js.js
@@ -88,45 +88,48 @@ function usernameIsValid(username) {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////signup form submitting interface
 
+//Boostrap input fields validations
+// starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
 
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                // Additional validation for the file input
+                const profileImageInput = document.getElementById('signup_profile_image');
+                const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+                const maxFileSize = 5 * 1024 * 1024; // 5MB
 
+                if (profileImageInput.files.length > 0) {
+                    const file = profileImageInput.files[0];
+                    const fileName = file.name.toLowerCase();
+                    const fileExtension = fileName.split('.').pop();
 
+                    if (allowedExtensions.includes(fileExtension) && file.size <= maxFileSize) {
+                        // validations done
+                        sendSignupDataIntoBackend();
+                    } else {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        profileImageInput.setCustomValidity('Please choose a valid image (JPG, JPEG, PNG, or GIF format) and ensure the file size is less than 5MB.');
+                    }
+                }
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
 
-// //Boostrap input fields validations
-// // starter JavaScript for disabling form submissions if there are invalid fields
-// (function () {
-//     'use strict';
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     var forms = document.querySelectorAll('.needs-validation');
-//
-//     // Loop over them and prevent submission
-//     Array.prototype.slice.call(forms).forEach(function (form) {
-//         form.addEventListener('submit', function (event) {
-//             if (!form.checkValidity()) {
-//                 event.preventDefault();
-//                 event.stopPropagation();
-//             } else {
-//                 // Additional validation for the file input
-//                 const profileImageInput = document.getElementById('signup_profile_image');
-//                 const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-//                 const maxFileSize = 5 * 1024 * 1024; // 5MB
-//
-//                 if (profileImageInput.files.length > 0) {
-//                     const file = profileImageInput.files[0];
-//                     const fileName = file.name.toLowerCase();
-//                     const fileExtension = fileName.split('.').pop();
-//
-//                     if (allowedExtensions.includes(fileExtension) && file.size <= maxFileSize) {
-//                         // validations done
-//
-//                     } else {
-//                         event.preventDefault();
-//                         event.stopPropagation();
-//                         profileImageInput.setCustomValidity('Please choose a valid image (JPG, JPEG, PNG, or GIF format) and ensure the file size is less than 5MB.');
-//                     }
-//                 }
-//             }
-//             form.classList.add('was-validated');
-//         }, false);
-//     });
-// })();
+//send data
+function sendSignupDataIntoBackend(){
+    console.log("done");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
