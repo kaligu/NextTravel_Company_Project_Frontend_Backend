bfe477d27dc72fda90db7227c331802889063583
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-21 07:54:39 +0530

    implimenting UserServiceController to API GAteway module

--- Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/api/UserServiceController.java
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/api/UserServiceController.java
@@ -6,10 +6,18 @@
 */
 package lk.nexttravel.api_gateway.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : H.C.Kaligu Jayanath
  * Date    : 10/21/2023
  * Time    : 7:52 AM
  */
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:63342/")
 public class UserServiceController {
 }
