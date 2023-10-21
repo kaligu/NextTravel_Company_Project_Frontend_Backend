330ba2ec855da7c72c39f579b27ad8dcd0bcc6c9
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-21 07:53:12 +0530

    add UserServiceController to API GAteway module

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/api/UserServiceController.java
@@ -0,0 +1,15 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/21/2023
+  @ Time         : 7:52 AM
+*/
+package lk.nexttravel.api_gateway.api;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/21/2023
+ * Time    : 7:52 AM
+ */
+public class UserServiceController {
+}
