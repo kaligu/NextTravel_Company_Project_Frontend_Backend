f426aa991f1e72a00c555c1f53cb37a2bde79e52
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-21 07:45:01 +0530

    add admin entity class to User_MicroService module

---
+++ Backend/User_MicroService/src/main/java/lk/nexttravel/user_microservice/entity/Admin.java
@@ -0,0 +1,15 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/21/2023
+  @ Time         : 7:44 AM
+*/
+package lk.nexttravel.user_microservice.entity;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/21/2023
+ * Time    : 7:44 AM
+ */
+public class Admin {
+}
--- Backend/User_MicroService/src/main/java/lk/nexttravel/user_microservice/entity/Client.java
+++ Backend/User_MicroService/src/main/java/lk/nexttravel/user_microservice/entity/Client.java
@@ -6,10 +6,26 @@
 */
 package lk.nexttravel.user_microservice.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * @author : H.C.Kaligu Jayanath
  * Date    : 10/21/2023
  * Time    : 7:41 AM
  */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
+
+@Document(collection = "Client")
 public class Client {
+    @Id
+    private String id;
+    private String nic_or_passport;
+    private String address;
 }
