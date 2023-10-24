440da5ce403d16112f18442fb173a0168f28965a
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-25 01:12:13 +0530

    added model maper config class to User Micro Service

---
+++ Backend/User_MicroService/src/main/java/lk/nexttravel/user_microservice/config/ModelMapperConfig.java
@@ -0,0 +1,25 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/25/2023
+  @ Time         : 12:59 AM
+*/
+package lk.nexttravel.user_microservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/25/2023
+ * Time    : 12:59 AM
+ */
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
