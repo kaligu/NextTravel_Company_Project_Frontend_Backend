feb544ea75ea8957f2c416285e28682287a7e42b
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-25 23:01:39 +0530

    implimenting frontend for loginup navigations

--- Frontend/pages/assets/js/login_page_js.js
+++ Frontend/pages/assets/js/login_page_js.js
@@ -194,7 +194,7 @@ function loginFormBtnClicked() {
                         }, 1500); // 2000 milliseconds
                     });
 
-                }else if(data.data === RoleTypes.ROLE_ADMIN_SERVICE_USER){
+                }else if(data.data === RoleTypes.ROLE_ADMIN_SERVICE_GUIDE){
 
                     loadingModel.on('hidden.bs.modal', function () {
                         // Show alert after the modal is completely hidden
@@ -207,7 +207,7 @@ function loginFormBtnClicked() {
                         loadingModel.off('hidden.bs.modal');
 
                         setTimeout(function () {
-                            window.location.href = 'http://localhost:63342/NextTravel_Company_Project_Frontend_Backend/Frontend/pages/user_admin_main_page.html?_ijt=lgk33b09l42ffpce17ruvb5qhv&_ij_reload=RELOAD_ON_SAVE';
+                            window.location.href = 'http://localhost:63342/NextTravel_Company_Project_Frontend_Backend/Frontend/pages/guide_admin_main_page.html?_ijt=1mvkd5bqbnv72hebvfm2dsuead&_ij_reload=RELOAD_ON_SAVE';
                         }, 1500); // 2000 milliseconds
                     });
 
