d4c9cd8306455dff483fdee0ff6910d3023630f7
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-25 07:24:27 +0530

    added command line runner to api gateway for testing

--- Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/ApiGatewayApplication.java
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,41 @@
 package lk.nexttravel.api_gateway;
 
+import lk.nexttravel.api_gateway.Persistence.UserRepository;
+import lk.nexttravel.api_gateway.entity.User;
+import lk.nexttravel.api_gateway.service.SequenceGeneratorService;
+import lk.nexttravel.api_gateway.util.RoleTypes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    SequenceGeneratorService sequenceGeneratorService;
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(){
+        return args -> {
+            userRepository.save(
+                    User.builder()
+                            .id("U00"+sequenceGeneratorService.generateSequence(User.SEQUENCE_NAME))
+                            .email(" chethiyakaligu2@gmail.com")
+                            .name("Admin Test")
+                            .password("1234")
+                            .role_type(RoleTypes.ROLE_ADMIN_SERVICE_USER)
+                            .build()
+            );
+        };
     }
 
 }
