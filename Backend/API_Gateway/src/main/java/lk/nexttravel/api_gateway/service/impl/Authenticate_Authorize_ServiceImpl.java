7bd37883121b3bcb6876328e4f4d97d16f2f07d9
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-27 04:57:22 +0530

    add Authenticate_Authorize_ServiceImpl class into api gateway

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/service/impl/Authenticate_Authorize_ServiceImpl.java
@@ -0,0 +1,15 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/27/2023
+  @ Time         : 4:56 AM
+*/
+package lk.nexttravel.api_gateway.service.impl;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/27/2023
+ * Time    : 4:56 AM
+ */
+public class Authenticate_Authorize_ServiceImpl {
+}
