5b29d84e296660a119fe713b80f401bcc90a2a25
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-27 11:30:36 +0530

    implimenting Frontend - pswd recovery option

--- Frontend/pages/assets/js/login_pswd_recovery_page_js.js
+++ Frontend/pages/assets/js/login_pswd_recovery_page_js.js
@@ -4,13 +4,13 @@ const login_container = $('#login_container');
 
 const intrologin_pg_loadingModel = $('#intro-login-pg-loading-model');
 
-const intrologin_pg_alertModelError = $('#intro-login-pg-alert-model-error');
-const intrologin_pg_alertModelError_title =  $('#intro-login-pg-alert-model-title-error');
-const intrologin_pg_alertModelError_content = $('#intro-login-pg-alert-model-content-error');
+const pswdrecover_pg_alertModelError = $('#pswdrecover-pg-alert-model-error');
+const pswdrecover_pg_alertModelError_title =  $('#pswdrecover-pg-alert-model-title-error');
+const pswdrecover_pg_alertModelError_content = $('#pswdrecover-pg-alert-model-content-error');
 
-const intrologin_pg_alertModelDone = $('#intro-login-pg-alert-model-done');
-const intrologin_pg_alertModelDone_title =  $('#intro-login-pg-alert-model-title-done');
-const intrologin_pg_alertModelDone_content = $('#intro-login-pg-alert-model-content-done');
+const pswdrecover_pg_alertModelDone = $('#pswdrecover-pg-alert-model-done');
+const pswdrecover_pg_alertModelDone_title =  $('#pswdrecover-pg-alert-model-title-done');
+const pswdrecover_pg_alertModelDone_content = $('#pswdrecover-pg-alert-model-content-done');
 
 const pswd_recovery_login_username_txtfld = $('#pswd_recovery_login_username');
 const pswd_recovery_login_username_container = $('#forgot_pswd_get_username_container');
