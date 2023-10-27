2cad9bb8f4e5c7a9334769a16740bf9e21ae1b82
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-27 22:03:30 +0530

    implimenting frontend - verify otp with username

--- Frontend/pages/assets/js/login_pswd_recovery_page_js.js
+++ Frontend/pages/assets/js/login_pswd_recovery_page_js.js
@@ -20,7 +20,9 @@ const pswdrecover_pg_forgot_pswd_get_username_container = $('#forgot_pswd_get_us
 const pswdrecover_pg_pswdadding_model = $('#pswrdrecover-pg-pswdadding-model');
 
 let pswd_recovery_login_username_txtfld_value;
-let pswd_recovery_login_otp_txtfld_value;
+
+const pswd_recovery_login_not_confirm_pswd_txtfld = $('#pswrdrecover-pg-pswdadding-model-new-pswd-txtfld');
+const pswd_recovery_login_confirmed_pswd_txtfld = $('#pswrdrecover-pg-pswdadding-model-confiremed-pswd-txtfld');
 
 //////--------------------------password recovery username
 
@@ -54,7 +56,7 @@ function pswdRecoveryUsernameSearchBtnClicked(){
                             pswdrecover_pg_forgot_pswd_get_username_container.css('display','none');
                             pswdrecover_pg_forgot_pswd_get_otp_container.css('display','block');
 
-                        }, 3300); // delay
+                        }, 3000); // delay
                     } else {
                         pswdrecover_pg_alertModelError_title.text("Not Found!");
                         pswdrecover_pg_alertModelError_content.text("This User not Registerd yet.Try again or register please!");
@@ -163,19 +165,10 @@ function pswdRecoveryCreatePswdAndLogin(){
     pswdrecover_pg_pswdadding_model.modal('hide');
     pswdrecover_pg_loadingModel.modal('show');
 
-    let pswd_recovery_login_pswd_txtfld_value;
-
-    //confiremed and enterd password if matched
-    if(
-        $('#pswrdrecover-pg-pswdadding-model-new-pswd-txtfld').val() === $('#pswrdrecover-pg-pswdadding-model-confiremed-pswd-txtfld').val()
-    ){
-        pswd_recovery_login_pswd_txtfld_value=$('#pswrdrecover-pg-pswdadding-model-confiremed-pswd-txtfld').val()
-    }
-
     $.ajax({
         method: "GET",
         async: true,
-        url: 'http://localhost:1010/main/user/user-login-with-recoverd-password?username=' + encodeURIComponent(pswd_recovery_login_username_txtfld_value)+'&otp='+encodeURIComponent(pswd_recovery_login_otp_txtfld_value)+'&password='+encodeURIComponent(pswd_recovery_login_pswd_txtfld_value),
+        url: 'http://localhost:1010/main/user/user-login-with-recoverd-password?username=' + encodeURIComponent(pswd_recovery_login_username_txtfld_value)+'&otp='+encodeURIComponent(pswd_recovery_login_otp_txtfld_value)+'&password='+encodeURIComponent(pswd_recovery_login_confirmed_pswd_txtfld.val()),
         processData: false,
         contentType: false,
         success: function (data) {
@@ -206,7 +199,7 @@ function pswdRecoveryCreatePswdAndLogin(){
                                         pswdrecover_pg_alertModelDone.modal('hide');
 
                                         window.location.href = 'http://localhost:63342/NextTravel_Company_Project_Frontend_Backend/Frontend/pages/client_main_page.html?_ijt=lgk33b09l42ffpce17ruvb5qhv&_ij_reload=RELOAD_ON_SAVE';
-                                    }, 3000); // delay
+                                    }, 2500); // delay
 
                                 }, 100); // delay
 
@@ -349,3 +342,85 @@ function pswdRecoveryCreatePswdAndLogin(){
         }
     });
 }
+
+//-------------- validations ----------------------------------
+//usernam txt fld validation
+function iisUsernameCheckedRegexLogin(username) {
+    const regex = /^[a-zA-Z0-9_.-]{5,30}$/;
+    return regex.test(username);
+}
+pswd_recovery_login_username_txtfld.on('keyup', function () {
+    const wtinvalidFeedback = pswd_recovery_login_username_txtfld.siblings('.invalid-feedback');
+    if (iisUsernameCheckedRegexLogin(pswd_recovery_login_username_txtfld.val())) {
+        pswd_recovery_login_username_txtfld.removeClass('is-invalid');
+        wtinvalidFeedback.hide();
+        $('#pswd_recovery_login_search_btn').prop("disabled", false);
+    } else {
+        pswd_recovery_login_username_txtfld.addClass('is-invalid');
+        wtinvalidFeedback.show();
+        $('#pswd_recovery_login_search_btn').prop("disabled", true);
+    }
+});
+
+//usernam txt fld validation
+function isUsernameOTPRegexLogin(username) {
+    const regex = /^\b\d{4}\b$/;
+    return regex.test(username);
+}
+pswd_recovery_login_otp_txtfld.on('keyup', function () {
+    const wtinvalidFeedback = pswd_recovery_login_otp_txtfld.siblings('.invalid-feedback');
+    if (isUsernameOTPRegexLogin(pswd_recovery_login_otp_txtfld.val())) {
+        pswd_recovery_login_otp_txtfld.removeClass('is-invalid');
+        wtinvalidFeedback.hide();
+        $('#pswd_recovery_login_otp_btn').prop("disabled", false);
+    } else {
+        pswd_recovery_login_otp_txtfld.addClass('is-invalid');
+        wtinvalidFeedback.show();
+        $('#pswd_recovery_login_otp_btn').prop("disabled", true);
+    }
+});
+
+//password txt fld validation
+function isUsernamepswdRegexLogin(pswd) {
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return regex.test(pswd);
+}
+
+pswd_recovery_login_not_confirm_pswd_txtfld.on('keyup', function () {
+    const tinvalidFeedback = pswd_recovery_login_not_confirm_pswd_txtfld.siblings('.invalid-feedback');
+    const tvalidFeedback = pswd_recovery_login_not_confirm_pswd_txtfld.siblings('.valid-feedback');
+    if (isUsernamepswdRegexLogin(pswd_recovery_login_not_confirm_pswd_txtfld.val())
+        && pswd_recovery_login_not_confirm_pswd_txtfld.val() === pswd_recovery_login_confirmed_pswd_txtfld.val()
+    ) {
+        pswd_recovery_login_not_confirm_pswd_txtfld.removeClass('is-invalid');
+        pswd_recovery_login_not_confirm_pswd_txtfld.addClass('is-valid');
+        tvalidFeedback.show();
+        tinvalidFeedback.hide();
+        $('#recover_pg_login_btn').prop("disabled", false);
+    } else {
+        pswd_recovery_login_not_confirm_pswd_txtfld.removeClass('is-valid');
+        pswd_recovery_login_not_confirm_pswd_txtfld.addClass('is-invalid');
+        tinvalidFeedback.show();
+        tvalidFeedback.hide();
+        $('#recover_pg_login_btn').prop("disabled", true);
+    }
+});
+pswd_recovery_login_confirmed_pswd_txtfld.on('keyup', function () {
+    const tinvalidFeedback = pswd_recovery_login_confirmed_pswd_txtfld.siblings('.invalid-feedback');
+    const tvalidFeedback = pswd_recovery_login_confirmed_pswd_txtfld.siblings('.valid-feedback');
+    if (isUsernamepswdRegexLogin(pswd_recovery_login_confirmed_pswd_txtfld.val())
+    && pswd_recovery_login_not_confirm_pswd_txtfld.val() === pswd_recovery_login_confirmed_pswd_txtfld.val()
+    ) {
+        pswd_recovery_login_confirmed_pswd_txtfld.removeClass('is-invalid');
+        pswd_recovery_login_confirmed_pswd_txtfld.addClass('is-valid');
+        tvalidFeedback.show();
+        tinvalidFeedback.hide();
+        $('#recover_pg_login_btn').prop("disabled", false);
+    } else {
+        pswd_recovery_login_confirmed_pswd_txtfld.removeClass('is-valid');
+        pswd_recovery_login_confirmed_pswd_txtfld.addClass('is-invalid');
+        tinvalidFeedback.show();
+        tvalidFeedback.hide();
+        $('#recover_pg_login_btn').prop("disabled", true);
+    }
+});
