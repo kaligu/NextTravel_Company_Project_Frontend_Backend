e47016930ad9c1b91dc336a5b36d749ef7c60db6
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-27 23:00:35 +0530

    add UserServiceController class into apigateway

--- Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/api/UserServiceController.java
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/api/UserServiceController.java
@@ -6,9 +6,13 @@
 */
 package lk.nexttravel.api_gateway.api;
 
+import lk.nexttravel.api_gateway.dto.RespondDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * @author : H.C.Kaligu Jayanath
@@ -17,7 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user-service")
 @CrossOrigin(origins = "http://localhost:63342/")
 public class UserServiceController {
+
+    //user login
+    @GetMapping(value = "/user-login")
+    public Mono<ResponseEntity<RespondDTO>> getUsernameAndProfileImage(FrontendTokenDTO frontendTokenDTO){
+        InternalFrontendSecurityCheckDTO internalFrontendSecurityCheckDTO = authenticate_authorize_service.validateRequestsAndGetMetaData(frontendTokenDTO);
+        if(
+                internalFrontendSecurityCheckDTO.isAccesssible()
+                        &&
+                        internalFrontendSecurityCheckDTO.getRole().equals(Roliy)
+        ){
+
+        }else{
+
+        }
+    }
 }
