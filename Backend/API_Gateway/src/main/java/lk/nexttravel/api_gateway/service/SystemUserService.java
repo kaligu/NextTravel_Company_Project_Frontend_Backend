c4c9524db36f33f4140bf2aff545c94cf7286ca7
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-27 23:05:25 +0530

    add interface UserService into apigateway

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/service/SystemUserService.java
@@ -0,0 +1,38 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/20/2023
+  @ Time         : 4:50 AM
+*/
+package lk.nexttravel.api_gateway.service;
+
+import lk.nexttravel.api_gateway.dto.RespondDTO;
+import lk.nexttravel.api_gateway.dto.auth.UserSignupDTO;
+import lk.nexttravel.api_gateway.util.RoleTypes;
+import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/20/2023
+ * Time    : 4:50 AM
+ */
+public interface SystemUserService {
+
+    ResponseEntity<RespondDTO> ischeckUsernameAlreadyTaken(String username);
+
+    ResponseEntity<RespondDTO> checkUsernameAndSendOTP(String username);
+
+    ResponseEntity<RespondDTO> verifyUsernameWithOTP(String username, String otp);
+
+    ResponseEntity<RespondDTO> userLoginWithRecoverdPassword(String username, String otp, String password);
+
+    ResponseEntity<RespondDTO> saveNewGuestUser(UserSignupDTO userSignupDTO);
+
+    Mono<ResponseEntity<RespondDTO>> checkUsernamePasswordUserLogin(String username, String password);
+
+    void saveNewAdminUserOnlyTesting(UserSignupDTO userSignupDTO, RoleTypes roleTypes);
+
+
+//    Mono<ResponseEntity<RespondDTO>> getUserProfileImage(String username);
+}
--- Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/service/UserService.java
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/service/UserService.java
@@ -1,38 +1,15 @@
 /*
   @ Author       : C.Kaligu Jayanath
   @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
-  @ Date         : 10/20/2023
-  @ Time         : 4:50 AM
+  @ Date         : 10/27/2023
+  @ Time         : 11:04 PM
 */
 package lk.nexttravel.api_gateway.service;
 
-import lk.nexttravel.api_gateway.dto.RespondDTO;
-import lk.nexttravel.api_gateway.dto.auth.UserSignupDTO;
-import lk.nexttravel.api_gateway.util.RoleTypes;
-import org.springframework.http.ResponseEntity;
-import reactor.core.publisher.Mono;
-
 /**
  * @author : H.C.Kaligu Jayanath
- * Date    : 10/20/2023
- * Time    : 4:50 AM
+ * Date    : 10/27/2023
+ * Time    : 11:04 PM
  */
 public interface UserService {
-
-    ResponseEntity<RespondDTO> ischeckUsernameAlreadyTaken(String username);
-
-    ResponseEntity<RespondDTO> checkUsernameAndSendOTP(String username);
-
-    ResponseEntity<RespondDTO> verifyUsernameWithOTP(String username, String otp);
-
-    ResponseEntity<RespondDTO> userLoginWithRecoverdPassword(String username, String otp, String password);
-
-    ResponseEntity<RespondDTO> saveNewGuestUser(UserSignupDTO userSignupDTO);
-
-    Mono<ResponseEntity<RespondDTO>> checkUsernamePasswordUserLogin(String username, String password);
-
-    void saveNewAdminUserOnlyTesting(UserSignupDTO userSignupDTO, RoleTypes roleTypes);
-
-
-//    Mono<ResponseEntity<RespondDTO>> getUserProfileImage(String username);
 }
