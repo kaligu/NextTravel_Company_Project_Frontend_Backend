be11cebd1cf51223d826cb065a5810d7822e4eee
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-28 19:14:29 +0530

    implimenting load profile image to vehicle admin control panel

--- Frontend/pages/assets/js/main_js/vehicleadmin_main.js
+++ Frontend/pages/assets/js/main_js/vehicleadmin_main.js
@@ -1,8 +1,8 @@
 //-----------------varibales--------
-const user_admin_main_pg_loading_model = $('#user_admin_main_pg-loading-model');
+const vehicle_admin_main_pg_loading_model = $('#vehicle_admin_main_pg-loading-model');
 
-const user_admin_main_pg_profile_img = $("#user_admin_main_pg_profile_img");
-const user_admin_main_pg_top_admin_name = $("#user_admin_main_pg_top_admin_name")
+const vehicle_admin_main_pg_profile_img = $("#vehicle_admin_main_pg_profile_img");
+const vehicle_admin_main_pg_top_admin_name = $("#vehicle_admin_main_pg_top_admin_name")
 //---------------------------------------------
 
 //---------------load profile image and username---------------------
@@ -13,7 +13,7 @@ $(document).ready(function() {
     console.log(localStorage.getItem("secure_data_vehicle_admin_refresh_token"));
 
     //show loading model
-    user_admin_main_pg_loading_model.modal('show');
+    vehicle_admin_main_pg_loading_model.modal('show');
 
     $.ajax({
         method: "GET",
@@ -33,12 +33,12 @@ $(document).ready(function() {
                 localStorage.setItem("secure_data_vehicle_admin_refresh_token", data.token.access_refresh_token);
 
                 // Set image from base64 data
-                user_admin_main_pg_profile_img.attr('src', 'data:image/png;base64,' + data.data);
-                user_admin_main_pg_top_admin_name.text("Mr. "+data.token.access_username+" [Admin]");
+                vehicle_admin_main_pg_profile_img.attr('src', 'data:image/png;base64,' + data.data);
+                vehicle_admin_main_pg_top_admin_name.text("Mr. "+data.token.access_username+" [Admin]");
 
                 //hide loading model
                 setTimeout(function () {
-                    user_admin_main_pg_loading_model.modal('hide');
+                    vehicle_admin_main_pg_loading_model.modal('hide');
                 }, 1000); // delay
 
             } else {
