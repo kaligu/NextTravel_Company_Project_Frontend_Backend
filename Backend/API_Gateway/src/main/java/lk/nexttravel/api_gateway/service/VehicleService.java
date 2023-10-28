3431706f5f206538a8bad9e3923622467798c5d9
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-28 19:18:10 +0530

    implimenting load profile image to vehicle admin control panel - VehicleService interface added

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/service/VehicleService.java
@@ -0,0 +1,15 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/28/2023
+  @ Time         : 7:17 PM
+*/
+package lk.nexttravel.api_gateway.service;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/28/2023
+ * Time    : 7:17 PM
+ */
+public interface VehicleService {
+}
