26657ee38cd4454255819ca111a44cb648bfdcbb
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-28 13:53:51 +0530

    implimenting load image to user admin control panel

--- Frontend/pages/assets/js/useradmin/useradmin_main.js
+++ Frontend/pages/assets/js/useradmin/useradmin_main.js
@@ -43,14 +43,18 @@
 //         }
 //     });
 // }
-
+const user_admin_main_pg_loading_model = $('#user_admin_main_pg-loading-model');
 
 const user_admin_main_pg_profile_img = $("#user_admin_main_pg_profile_img");
+const user_admin_main_pg_top_admin_name = $("#user_admin_main_pg_top_admin_name")
 
 $(document).ready(function() {
-    console.log(localStorage.getItem("secure_data_username"));
-    console.log(localStorage.getItem("secure_data_access_token"));
-    console.log(localStorage.getItem("secure_data_refresh_token"));
+    // console.log(localStorage.getItem("secure_data_username"));
+    // console.log(localStorage.getItem("secure_data_access_token"));
+    // console.log(localStorage.getItem("secure_data_refresh_token"));
+
+    //show loading model
+    user_admin_main_pg_loading_model.modal('show');
 
     $.ajax({
         method: "GET",
@@ -71,6 +75,11 @@ $(document).ready(function() {
 
                 // Set image from base64 data
                 user_admin_main_pg_profile_img.attr('src', 'data:image/png;base64,' + data.data);
+                user_admin_main_pg_top_admin_name.text("Mr. "+data.token.access_username+" [Admin]");
+
+                //hide loading model
+                user_admin_main_pg_loading_model.modal('hide');
+
             } else {
                 console.log("Profile image retrieval failed");
             }
