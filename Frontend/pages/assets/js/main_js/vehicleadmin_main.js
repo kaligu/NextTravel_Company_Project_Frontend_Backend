8689c0818928075dfcd4fbe187ef456942f87fab
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-28 19:08:53 +0530

    implimenting load profile image to vehicle admin control panel

--- Frontend/pages/assets/js/main_js/vehicleadmin_main.js
+++ Frontend/pages/assets/js/main_js/vehicleadmin_main.js
@@ -8,9 +8,9 @@ const user_admin_main_pg_top_admin_name = $("#user_admin_main_pg_top_admin_name"
 //---------------load profile image and username---------------------
 
 $(document).ready(function() {
-    // console.log(localStorage.getItem("secure_data_username"));
-    // console.log(localStorage.getItem("secure_data_access_token"));
-    // console.log(localStorage.getItem("secure_data_refresh_token"));
+    console.log(localStorage.getItem("secure_data_vehicle_admin_username"));
+    console.log(localStorage.getItem("secure_data_vehicle_admin_access_token"));
+    console.log(localStorage.getItem("secure_data_vehicle_admin_refresh_token"));
 
     //show loading model
     user_admin_main_pg_loading_model.modal('show');
@@ -21,16 +21,16 @@ $(document).ready(function() {
         url: 'http://localhost:1010/main/user-service/user-admin-get-profile-image',
         async: true,
         data: {
-            access_username: localStorage.getItem("secure_data_username"),
-            access_jwt_token: localStorage.getItem("secure_data_access_token"),
-            access_refresh_token: localStorage.getItem("secure_data_refresh_token")
+            access_username: localStorage.getItem("secure_data_vehicle_admin_username"),
+            access_jwt_token: localStorage.getItem("secure_data_vehicle_admin_access_token"),
+            access_refresh_token: localStorage.getItem("secure_data_vehicle_admin_refresh_token")
         },
         success: function(data) {
             if (data.rspd_code === RespondCodes.Response_SUCCESS) {
                 // Save tokens to localStorage
-                localStorage.setItem("secure_data_username", data.token.access_username);
-                localStorage.setItem("secure_data_access_token", data.token.access_jwt_token);
-                localStorage.setItem("secure_data_refresh_token", data.token.access_refresh_token);
+                localStorage.setItem("secure_data_vehicle_admin_username", data.token.access_username);
+                localStorage.setItem("secure_data_vehicle_admin_access_token", data.token.access_jwt_token);
+                localStorage.setItem("secure_data_vehicle_admin_refresh_token", data.token.access_refresh_token);
 
                 // Set image from base64 data
                 user_admin_main_pg_profile_img.attr('src', 'data:image/png;base64,' + data.data);
