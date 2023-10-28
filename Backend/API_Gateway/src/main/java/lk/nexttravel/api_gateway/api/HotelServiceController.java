d8250bb5e66d20aa577f94d2a7e9c7dc181a9fd7
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-28 18:27:14 +0530

    implimenting load image to hotel admin control panel

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/api/HotelServiceController.java
@@ -0,0 +1,23 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/28/2023
+  @ Time         : 6:26 PM
+*/
+package lk.nexttravel.api_gateway.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/28/2023
+ * Time    : 6:26 PM
+ */
+
+@RestController
+@RequestMapping("/hotel-service")
+@CrossOrigin(origins = "http://localhost:63342/")
+public class HotelServiceController {
+}
