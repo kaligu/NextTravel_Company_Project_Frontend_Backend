9f4f79ac306de44764424157ca011084aee52997
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-28 19:19:08 +0530

    implimenting load profile image to vehicle admin control panel - VehicleServiceImpl class added

---
+++ Backend/API_Gateway/src/main/java/lk/nexttravel/api_gateway/service/impl/VehicleServiceImpl.java
@@ -0,0 +1,15 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/28/2023
+  @ Time         : 7:18 PM
+*/
+package lk.nexttravel.api_gateway.service.impl;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/28/2023
+ * Time    : 7:18 PM
+ */
+public class VehicleServiceImpl {
+}
