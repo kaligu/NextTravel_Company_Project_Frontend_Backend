7e2cdfc0c80eb0bcaea5a4dae00caa17a82bcbec
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-28 19:55:15 +0530

    implimenting load profile image to guide admin control panel - added GuideAdminController class into User Service

---
+++ Backend/User_MicroService/src/main/java/lk/nexttravel/user_microservice/api/GuideAdminController.java
@@ -0,0 +1,23 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/28/2023
+  @ Time         : 7:54 PM
+*/
+package lk.nexttravel.user_microservice.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/28/2023
+ * Time    : 7:54 PM
+ */
+
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin("*")
+public class GuideAdminController {
+}
