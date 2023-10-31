5d783aa48ab51148467e9ba393b21933236e64f1
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-31 20:38:33 +0530

    added Guide MicroService

---
+++ Backend/Guide_MicroService/src/main/java/lk/nexttravel/guide_microservice/GuideMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.guide_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideMicroServiceApplication.class, args);
+    }
+
+}
---
+++ Backend/Guide_MicroService/src/test/java/lk/nexttravel/guide_microservice/GuideMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.guide_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
