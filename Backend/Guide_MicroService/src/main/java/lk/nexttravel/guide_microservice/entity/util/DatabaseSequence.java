16c4d19a8bc108ff895af406bc7f85578a0a4595
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-31 20:56:21 +0530

    added Guide MicroService - DatabaseSequence class

---
+++ Backend/Guide_MicroService/src/main/java/lk/nexttravel/guide_microservice/entity/util/DatabaseSequence.java
@@ -0,0 +1,33 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/21/2023
+  @ Time         : 11:14 AM
+*/
+package lk.nexttravel.guide_microservice.entity.util;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/21/2023
+ * Time    : 11:14 AM
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
+
+@Document(collection = "database_sequences")
+@Component
+public class DatabaseSequence {
+    @Id
+    private String id;
+
+    private long seq;
+}
