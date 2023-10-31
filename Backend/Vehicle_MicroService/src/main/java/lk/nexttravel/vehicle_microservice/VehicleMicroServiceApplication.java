87732b25431a8d723425cc99cbd612380c9fdbd1
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-31 20:42:17 +0530

    added Vehicle MicroService

---
+++ Backend/Vehicle_MicroService/src/main/java/lk/nexttravel/vehicle_microservice/VehicleMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.vehicle_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleMicroServiceApplication.class, args);
+    }
+
+}
---
+++ Backend/Vehicle_MicroService/src/test/java/lk/nexttravel/vehicle_microservice/VehicleMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.vehicle_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
