689993e9b41984b9b28a3c0f3f1dd28cd259f0f7
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-10-31 16:51:35 +0530

    added Hotel MicroService

---
+++ Backend/Hotel_MicroService/src/main/java/lk/nexttravel/hotel_microservice/HotelMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.hotel_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelMicroServiceApplication.class, args);
+    }
+
+}
---
+++ Backend/Hotel_MicroService/src/test/java/lk/nexttravel/hotel_microservice/HotelMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.hotel_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
