195a6ee6c3cd0e4ee60e16adc2b946bd6f9c68d8
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-11-01 23:07:42 +0530

    implimenting Hotel Micro Service - HotelOptions class

--- Backend/Hotel_MicroService/src/main/java/lk/nexttravel/hotel_microservice/entity/HotelOptions.java
+++ Backend/Hotel_MicroService/src/main/java/lk/nexttravel/hotel_microservice/entity/HotelOptions.java
@@ -2,7 +2,7 @@
   @ Author       : C.Kaligu Jayanath
   @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
   @ Date         : 11/1/2023
-  @ Time         : 9:47 PM
+  @ Time         : 11:02 PM
 */
 package lk.nexttravel.hotel_microservice.entity;
 
@@ -11,39 +11,26 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 /**
  * @author : H.C.Kaligu Jayanath
  * Date    : 11/1/2023
- * Time    : 9:47 PM
+ * Time    : 11:02 PM
  */
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Builder
 
-@Document(collection = "Hotel")
-public class Hotel {
+@Document(collection = "HotelOptions")
+public class HotelOptions {
     @Id
     private String id;
-    private String location;
-    private String location_coordinates;
-    private int category_stars;
+    private String description;
     private String name;
-    private String remarks;
-    private int perday_hotel_fee_for_days;
-    private int perday_hotel_fee_for_nights;
-    private String email;
-    private boolean is_pet_allowed;
-    private int cancellation_fee;
-    private String tell_1;
-    private String tell_2;
-    private String reference_link;
     private String image;
+    private String fee;
 
     @DBRef
-    private List<HotelOptions> options;
+    private Hotel hotel;
 }
