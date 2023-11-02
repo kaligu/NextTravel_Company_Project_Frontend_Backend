9b295fa59e07ab7b3793b031acd6218aa36bd870
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-11-02 05:36:33 +0530

    implimenting Guide Micro Service

---
+++ Backend/Guide_MicroService/src/main/java/lk/nexttravel/guide_microservice/service/impl/GuideServiceImpl.java
@@ -0,0 +1,17 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 11/2/2023
+  @ Time         : 5:35 AM
+*/
+package lk.nexttravel.guide_microservice.service.impl;
+
+import lk.nexttravel.guide_microservice.service.GuideService;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 11/2/2023
+ * Time    : 5:35 AM
+ */
+public class GuideServiceImpl implements GuideService {
+}
