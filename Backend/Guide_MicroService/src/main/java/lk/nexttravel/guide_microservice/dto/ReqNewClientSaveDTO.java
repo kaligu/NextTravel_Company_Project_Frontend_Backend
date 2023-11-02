9a23e7f078d79fbca0901b1f47be46f389f9f2df
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-11-02 05:38:59 +0530

    implimenting Guide Micro Service

---
+++ Backend/Guide_MicroService/src/main/java/lk/nexttravel/guide_microservice/dto/ReqNewClientSaveDTO.java
@@ -0,0 +1,36 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 10/21/2023
+  @ Time         : 2:54 PM
+*/
+package lk.nexttravel.guide_microservice.dto;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 10/21/2023
+ * Time    : 2:54 PM
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+@Component
+public class ReqNewClientSaveDTO {
+    @Id
+    private String id;
+    private String name_with_initial;
+    private String address;
+    private String profile_image;
+    private String nic_or_passport;
+
+    //---for security purpose
+    private String token;
+}
--- Backend/Guide_MicroService/src/main/java/lk/nexttravel/guide_microservice/service/GuideService.java
+++ Backend/Guide_MicroService/src/main/java/lk/nexttravel/guide_microservice/service/GuideService.java
@@ -6,10 +6,17 @@
 */
 package lk.nexttravel.guide_microservice.service;
 
+import org.springframework.http.ResponseEntity;
+
 /**
  * @author : H.C.Kaligu Jayanath
  * Date    : 11/2/2023
  * Time    : 5:35 AM
  */
 public interface GuideService {
+    ResponseEntity<String> SaveNewClient_Prepare(ReqNewClientSaveDTO reqNewClientSaveDTO);
+
+    ResponseEntity<String> SaveNewClient_Commit(ReqNewClientSaveDTO reqNewClientSaveDTO);
+
+    ResponseEntity<String> SaveNewClient_Abrot(ReqNewClientSaveDTO reqNewClientSaveDTO);
 }
