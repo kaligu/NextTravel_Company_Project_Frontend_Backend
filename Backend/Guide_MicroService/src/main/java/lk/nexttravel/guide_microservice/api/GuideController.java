60fc45374d729fcb6b1e06676c203de00735a9d2
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-11-02 05:34:49 +0530

    implimenting Guide Micro Service

---
+++ Backend/Guide_MicroService/src/main/java/lk/nexttravel/guide_microservice/api/GuideController.java
@@ -0,0 +1,15 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 11/2/2023
+  @ Time         : 5:34 AM
+*/
+package lk.nexttravel.guide_microservice.api;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 11/2/2023
+ * Time    : 5:34 AM
+ */
+public class GuideController {
+}
