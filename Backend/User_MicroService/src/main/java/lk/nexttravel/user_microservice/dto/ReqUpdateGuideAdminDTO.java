5268c37e3bca09d1729fc557ebe8164a57d86c5f
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-11-02 09:24:15 +0530

    implimenting API Gateway

---
+++ Backend/User_MicroService/src/main/java/lk/nexttravel/user_microservice/dto/ReqUpdateGuideAdminDTO.java
@@ -0,0 +1,34 @@
+/*
+  @ Author       : C.Kaligu Jayanath
+  @ Prjoect Name : NextTravel_Company_Project_Frontend_Backend
+  @ Date         : 11/2/2023
+  @ Time         : 9:03 AM
+*/
+package lk.nexttravel.user_microservice.dto;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : H.C.Kaligu Jayanath
+ * Date    : 11/2/2023
+ * Time    : 9:03 AM
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+@Component
+public class ReqUpdateGuideAdminDTO {
+    private String signup_name_with_initial;
+    private String nic_or_passport;
+    private String address;
+    private String profile_image;
+    private String transaction_state;
+
+    //---for security purpose
+    private String token;
+}
